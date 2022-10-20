09c0997f2be5b97516196130433d3906849c56e7
Matthew <mattj20@gmail.com>
2022-10-19 19:28:53 -0700

    commit tree e9f6680b5a0ee10478d015230353dca2d5b71573

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 62
+let num2 = 1
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random())
+
+let num3 = 16
+let num4 = 6
+let mod = num3%num4
+
+let max = Math.max(0, 5, 12, 20)
